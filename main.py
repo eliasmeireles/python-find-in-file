7a23e4beaac674a123e842f85aca8444331dc280
Elias Meireles <elias.ferreira@groupsoftware.com.br>
2021-01-20 11:23:59 -0300

    Imprime os nome dos arquivos onde foi encontrado o valor informado.

--- main.py
+++ main.py
@@ -5,23 +5,26 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import glob
 import os
+import re
 
 
-def find_in_file(file_dir, file_extension, value_find_by):
+def find_in_file(file_dir, value_find_by):
     os.chdir(file_dir)
-    for log_file in glob.glob("*.{0}".format(file_extension)):
-        with open(log_file) as log:
-            if value_find_by in log.read():
-                print(log_file)
-        print("\n-------------------------------\n")
+    for log_file in glob.glob("*.*"):
+        occurrence_found = False
+
+        with open(log_file, 'r') as log:
+            log_content_lines = log.read()
+            if value_find_by in log_content_lines:
+                occurrence_found = True
+
+        if occurrence_found:
+            print(log_file)
+            print("\n-------------------------------\n")
 
 
 print "Informe o diretório dos arquivos:",
 file_dir = raw_input()
-
-print "Informe a extensão dos arquivos:",
-file_extension = raw_input()
-
 
 program_end = '-1'
 
@@ -29,6 +32,5 @@ while program_end != '0':
     print "Informe o valor a pesquisar nos arquivos:",
     value_find_by = raw_input()
     program_end = value_find_by
-    find_in_file(file_dir, file_extension, value_find_by)
-    print "Para finalizar, basta digitar 0:",
-
+    find_in_file(file_dir, value_find_by)
+    print "Para finalizar, basta digitar 0 |",
